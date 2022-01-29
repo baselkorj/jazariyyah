1d3d54f30319e31d2ae1bc36f3f54e9b1a186e95
baselkorj <baselanwarkorj@gmail.com>
2022-01-29 16:09:10 +0400

    refactored widgets outside file

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jazariyyah/content/strings.dart';
+import 'package:jazariyyah/widgets/bottom_nav_var.dart';
+import 'package:jazariyyah/widgets/chapters_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,29 +15,11 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            itemCount: chapters.length,
-            itemBuilder: (context, index) {
-              return Card(
-                child: ListTile(
-                  title: Text(chapters.values.toList()[index]),
-                ),
-              );
-            }),
+      body: const Padding(
+        padding: EdgeInsets.all(12.0),
+        child: ChaptersList(),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-              icon: const Icon(Icons.book), label: ui['chapters']),
-          BottomNavigationBarItem(
-              icon: const Icon(Icons.edit), label: ui['notes']),
-          BottomNavigationBarItem(
-              icon: const Icon(Icons.bookmark), label: ui['bookmarks'])
-        ],
-      ),
+      bottomNavigationBar: const BottomNavBar(),
     );
   }
 }
